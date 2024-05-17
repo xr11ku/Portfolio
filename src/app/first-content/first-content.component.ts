bc08bf1fee7f5d273f56ae36b96159cb6da9bdaf
unknown <xrikuxriku@gmail.com>
2024-05-17 14:35:53 +0400

    add

--- src/app/first-content/first-content.component.ts
+++ src/app/first-content/first-content.component.ts
@@ -7,7 +7,7 @@ import { Component } from '@angular/core';
 })
 export class FirstContentComponent {
 // ~ Text Typing Logic
-textToType: string = "Junior Front-End";
+textToType: string = "Junr Front-End";
 typedText: string = "";
 currentIndex: number = 0;
 typingSpeed: number = 150;
